45cbbc8c5475a1931bef5d5433db85f7df40c229
Felix Sarnthein <safelix@ethz.ch>
2023-01-13 18:25:19 +0100

    change load_data interface

--- losslandscape.py
+++ losslandscape.py
@@ -94,7 +94,11 @@ class ParamProjector():
         raise ValueError('Cannot infer whether to project or map input.')
 
 
-def load_data(config, batchsize, num_workers, pin_memory) -> Tuple[DataLoader, DataLoader]:
+def load_data(identifier:str, batchsize:int, num_workers:int, pin_memory:bool) -> Tuple[DataLoader, DataLoader]:
+    ckpt_path, name = identifier.split(':')
+    config =  Configuration.from_json(os.path.join(os.path.dirname(ckpt_path), 'config.json'))
+    config.mc_spec = create_mc_spec(config)
+    
     DSet = get_dataset(config)
     trfm = transforms.Compose([ # self-training
                     transforms.Lambda(lambda img: img.convert('RGB')), transforms.ToTensor(),
@@ -214,9 +218,6 @@ def eval_coords(coords:torch.Tensor, args):
     device = torch.device('cpu' if args['force_cpu'] else U.pick_single_gpu())
     coords = coords.to(device)
 
-    t_config =  Configuration.from_json(os.path.join(os.path.dirname(args['vec0']), 'config.json'))
-    t_config.mc_spec = create_mc_spec(t_config)
-
     # Setup ParamProjector.
     print('Loading models...')
     fnames = [args['vec0'], args['vec1'], args['vec2']]
@@ -237,7 +238,7 @@ def eval_coords(coords:torch.Tensor, args):
     torch.save(torch.stack([P(vec) for vec in vecs]), os.path.join(args['dir'], f'vecs.pt'))
 
     # DINO and Data Setup.
-    train_dl, valid_dl = load_data(t_config, args['batchsize'], args['num_workers'], not args['force_cpu'])
+    train_dl, valid_dl = load_data(args['vec0'], args['batchsize'], args['num_workers'], not args['force_cpu'])
 
     # Probing Setup
     analyses = {}
