5e3c64b9cf40a2f38cc8760b2fe3ac3503db9763
Felix Sarnthein <safelix@ethz.ch>
2023-07-07 20:25:05 +0200

    typo

--- dino.py
+++ dino.py
@@ -5,7 +5,7 @@ import time
 
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader, RandomSampler
 from torchinfo import summary
