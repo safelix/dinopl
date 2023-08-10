5a9dff33eb030d96b730470789a363b1a82654d6
Felix Sarnthein <safelix@ethz.ch>
2023-08-10 16:53:47 +0200

    fix mnist error

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -16,6 +16,29 @@ class MNIST(BaseDataset, datasets.MNIST):
     mean = (0.1307, 0.1307, 0.1307)  # = (self.data / 255).mean()
     std = (0.3081, 0.3081, 0.3081)  # = (self.data / 255).std()
 
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], int(self.targets[index])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img.numpy(), mode="L")
+        img = img.convert('RGB') # convert to RGB before transforms
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
 # run with 'python -m datasets.mnist'
 if __name__ == '__main__':
     path_to_data = os.environ['DINO_DATA']
