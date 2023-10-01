89e29a19854342597a71b96e6ce2d7869c29a064
Felix Sarnthein <safelix@student.ethz.ch>
2023-10-02 01:52:41 +0200

    add sklearn LDA and LogReg as probes

--- dinopl/probing.py
+++ dinopl/probing.py
@@ -15,11 +15,10 @@ from tqdm import tqdm
 __all__ = [
     'LinearAnylsis',
     'KNNAnalysis'
+    'LogRegAnalysis'
+    'LinDiscrAnalysis'
     'Prober'
 ]
-
-## TODO: Support for LinearDiscriminantAnalysis
-# https://scikit-learn.org/stable/modules/array_api.html#pytorch-support
 
 class Analysis(object):
     def prepare(self, n_features:int, n_classes:int, device:torch.device=None, generator:torch.Generator=None) -> None:   
@@ -159,6 +158,97 @@ class KNNAnalysis(Analysis):
         self.labels = None
         self.acc = None
         self.device = torch.device('cpu')
+
+
+class LogRegAnalysis(Analysis):
+    def __init__(self):
+        super().__init__()
+        self.clf = None
+        self.opt = None
+        self.acc = None
+        self.device = torch.device('cpu')
+
+    @torch.inference_mode(False)
+    @torch.no_grad() 
+    def prepare(self, n_features:int, n_classes:int, device:torch.device=None, generator:torch.Generator=None):   
+        from sklearn.linear_model import LogisticRegression
+        self.clf = LogisticRegression(multi_class='multinomial')
+        self.acc = Accuracy(task='multiclass', num_classes=n_classes)
+
+    @torch.inference_mode(False)
+    @torch.no_grad()
+    def train(self, train_data:List[Tuple[torch.Tensor, torch.Tensor]], verbose=True):
+        X, y = zip(*train_data)
+        X, y = torch.cat(X).numpy(), torch.cat(y).numpy()
+        self.clf.fit(X, y)
+
+    def valid(self, valid_data:List[Tuple[torch.Tensor, torch.Tensor]]):
+        valid_pbar = tqdm(valid_data, leave=False)
+        valid_pbar.set_description('Validation')
+
+        self.acc.reset()
+        for embeddings, targets in valid_pbar:
+            predictions = self.clf.predict_proba(embeddings.numpy())
+            self.acc.update(torch.from_numpy(predictions), targets)
+            valid_pbar.set_postfix({'acc':float(self.acc.compute())})
+
+        return float(self.acc.compute())
+
+    def cleanup(self):
+        self.clf = None
+        self.acc = None
+        self.device = torch.device('cpu')
+
+# LinearDiscriminantAnalysis supported on GPU:
+# https://scikit-learn.org/stable/modules/array_api.html#pytorch-support
+class LinDiscrAnalysis(Analysis):
+    from sklearn import config_context
+    
+    def __init__(self):
+        super().__init__()
+        self.clf = None
+        self.opt = None
+        self.acc = None
+        self.device = torch.device('cpu')
+
+    @torch.inference_mode(False)
+    @torch.no_grad() 
+    @config_context(array_api_dispatch=True)
+    def prepare(self, n_features:int, n_classes:int, device:torch.device=None, generator:torch.Generator=None):   
+        from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+        self.clf = LDA()
+        self.acc = Accuracy(task='multiclass', num_classes=n_classes)
+
+        if device and device.type == 'cuda':
+            self.acc = self.acc.to(device=device)
+            self.device = device
+
+    @torch.inference_mode(False)
+    @torch.no_grad()
+    @config_context(array_api_dispatch=True)
+    def train(self, train_data:List[Tuple[torch.Tensor, torch.Tensor]], verbose=True):
+        X, y = zip(*train_data)
+        X, y = torch.cat(X), torch.cat(y)
+        self.clf.fit(X.to(self.device), y.to(self.device))
+
+    @config_context(array_api_dispatch=True)
+    def valid(self, valid_data:List[Tuple[torch.Tensor, torch.Tensor]]):
+        valid_pbar = tqdm(valid_data, leave=False)
+        valid_pbar.set_description('Validation')
+
+        self.acc.reset()
+        for embeddings, targets in valid_pbar:
+            predictions = self.clf.predict_proba(embeddings.to(self.device))
+            self.acc.update(predictions, targets.to(self.device))
+            valid_pbar.set_postfix({'acc':float(self.acc.compute())})
+
+        return float(self.acc.compute())
+
+    def cleanup(self):
+        self.clf = None
+        self.acc = None
+        self.device = torch.device('cpu')
+
 
 @torch.inference_mode()
 def load_data(encoder:nn.Module, dl:DataLoader, device:torch.device=None):
